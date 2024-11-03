c3ae7bf9679d25f364c848a37f8c1d6afeccc712
YAJATapps <yajatapps3@gmail.com>
2024-11-03 15:06:07 -0500

    Android Automotive Variant

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,11 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_apollo_p2.mk \
-    $(LOCAL_DIR)/omni_apollo_p2_tv.mk
+    $(LOCAL_DIR)/omni_apollo_p2_tv.mk \
+    $(LOCAL_DIR)/omni_apollo_p2_car.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_apollo_p2-user \
     omni_apollo_p2-userdebug \
     omni_apollo_p2-eng \
     omni_apollo_p2_tv-userdebug \
+    omni_apollo_p2_car-userdebug
---
+++ omni_apollo_p2_car.mk
@@ -0,0 +1,18 @@
+# Inherit from device.
+$(call inherit-product, device/allwinner/apollo-p2/device.mk)
+
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_CHARACTERISTICS := automotive,nosdcard
+
+PRODUCT_NAME := omni_apollo_p2_car
+
+# Device tree path.
+PRODUCT_PLATFORM_PATH := device/softwinner/apollo
+$(call inherit-product, $(PRODUCT_PLATFORM_PATH)/apollo-p2/camera/config.mk)
+$(call inherit-product, $(PRODUCT_PLATFORM_PATH)/common/*/config.mk)
+
+# Inherit from car
+$(call inherit-product, device/google_car/common/pre_google_car.mk)
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+$(call inherit-product, device/google_car/common/post_google_car.mk)
+
