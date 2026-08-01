742f61255fcbcef059dc413174aa79979d8f4ec8
YAJATapps <yajatapps3@gmail.com>
2026-08-01 14:34:23 -0400

    apollo-p2: Update AndroidProducts.mk lunch choices to trunk_staging format

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,8 +4,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_apollo_p2_car.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo_p2-user \
-    lineage_apollo_p2-userdebug \
-    lineage_apollo_p2-eng \
-    lineage_apollo_p2_tv-userdebug \
-    lineage_apollo_p2_car-userdebug
+    lineage_apollo_p2-trunk_staging-user \
+    lineage_apollo_p2-trunk_staging-userdebug \
+    lineage_apollo_p2-trunk_staging-eng \
+    lineage_apollo_p2_tv-trunk_staging-userdebug \
+    lineage_apollo_p2_car-trunk_staging-userdebug
